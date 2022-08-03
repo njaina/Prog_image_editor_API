aa54866f31ffaabcd19dd22ff2d414ddab5f0597
njaina <hei.njaina.2@gmail.com>
2022-08-03 22:38:14 +0300

    add src containts

---
+++ Java-spring-image-controller-main/src/main/java/com/imagecontroller/ImageControllerApplication.java
@@ -0,0 +1,13 @@
+package com.image.imagecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageControllerApplication.class, args);
+    }
+
+}
