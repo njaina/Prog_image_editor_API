2600809d62838864229ced966a95751ad155f6b9
njaina <hei.njaina.2@gmail.com>
2022-08-03 22:39:18 +0300

    add test containts

---
+++ Java-spring-image-controller-main/src/test/java/com/imagecontroller/ImageControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.image.imagecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
